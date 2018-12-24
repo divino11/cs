2f81d8958416a5916908691ad1e44ec46091acb6
Dmitriy Kondrashov <mitrich.kh@gmail.com>
2018-12-24 19:33:48 +0200

    Alert notification description

--- resources/views/emails/alert/triggered.blade.php
+++ resources/views/emails/alert/triggered.blade.php
@@ -2,7 +2,7 @@
 Alert {{ $alert->name }} has been triggered.
 
 @include('alert.description.' . $alert->type).
-The {{ App\Enums\AlertMetric::getKey((int)$alert->conditions['metric']) }} is currently {{ $value }}.
+The {{ App\Enums\AlertMetric::getDescription((int)$alert->conditions['metric']) }} is currently {{ $value }}.
 
 This alert has been triggered {{ $alert->triggerings_number }} out of {{ $alert->triggerings_limit }} times.
 @component('mail::button', ['url' => $disableUrl])
