8511aafd7d08e00d8d9aa22376e72a03d1ba4572
--get <a.vasyukov97@gmail.com>
2019-02-01 18:27:16 +0200

    Add transaction all operations

---
+++ app/Http/Controllers/User/ShowBillingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ShowBillingController extends Controller
+{
+    public function __invoke()
+    {
+        return view('user.billing', [
+            'transactions' => Transaction::where('user_id', Auth::user()->id)
+                ->order_by('created_at', 'desc')
+                ->paginate(10)
+        ]);
+    }
+}
---
+++ resources/views/user/billing.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.user.account')
+
+@section('title', 'Billing')
+
+@section('tabcontent')
+    <div class="container">
+        <div class="tab-pane active">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Billing History</h3>
+                    <hr>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr class="active">
+                            <th class="hidden-sm hidden-xs">ID</th>
+                            <th>Transaction Date</th>
+                            <th>Description</th>
+                            <th>Amount (USD)</th>
+                            <th>Service</th>
+                            <th>Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($transactions as $transaction)
+                            <tr>
+                                <td class="hidden-sm hidden-xs">
+                                    {{ $transaction->id }}
+                                </td>
+                                <td>
+                                    {{ $transaction->transaction_date }}
+                                </td>
+                                <td>
+                                    {{ $transaction->description }}
+                                </td>
+                                <td>
+                                    ${{ $transaction->amount }}
+                                </td>
+                                <td>
+                                    {{ $transaction->service }}
+                                </td>
+                                <td>
+                                    {{ $transaction->status }}
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6" class="text-center"><h2>Data is empty...</h2></td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                        <tfoot>
+                        <td colspan="6">
+                            <ul class="pagination pull-right">
+                                {{$transactions->links()}}
+                            </ul>
+                        </td>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
