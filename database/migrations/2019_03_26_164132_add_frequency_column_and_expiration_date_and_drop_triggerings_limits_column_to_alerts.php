5e67dd1c527f5dd5f2077f3d72f3350e0279be56
--get <a.vasyukov97@gmail.com>
2019-03-28 14:10:35 +0200

    Update migration for Alerts

--- database/migrations/2019_03_26_164132_add_frequency_column_and_expiration_date_and_drop_triggerings_limits_column_to_alerts.php
+++ database/migrations/2019_03_26_164132_add_frequency_column_and_expiration_date_and_drop_triggerings_limits_column_to_alerts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNewColumnToAlerts extends Migration
+class AddFrequencyColumnAndExpirationDateColumnAndDropTriggeringsLimitsColumnToAlerts extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,8 @@ class AddNewColumnToAlerts extends Migration
     public function up()
     {
         Schema::table('alerts', function (Blueprint $table) {
-            $table->boolean('frequency')->nullable();
-            $table->timestamp('expiration_date')->nullable();
+            $table->boolean('frequency')->nullable()->after('conditions');
+            $table->timestamp('expiration_date')->nullable()->after('frequency');
             $table->dropColumn('triggerings_limits');
         });
     }
@@ -28,7 +28,9 @@ class AddNewColumnToAlerts extends Migration
     public function down()
     {
         Schema::table('alerts', function (Blueprint $table) {
-            //
+            $table->dropColumn('frequency');
+            $table->dropColumn('expiration_date');
+            $table->integer('triggerings_limit')->after('triggerings_number');
         });
     }
 }
