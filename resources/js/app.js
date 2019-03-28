70e700ea5d563ede924df7adf5cbca1445cdd17f
--get <a.vasyukov97@gmail.com>
2019-03-28 13:52:19 +0200

    Browser Alerts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,9 +5,43 @@
  */
 
 require('./bootstrap');
+require('bootstrap-notify');
 window.Chosen = require('chosen-js');
 
 window.Vue = require('vue');
+
+import Echo from "laravel-echo"
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '825ad4919731c1c3e402',
+    cluster: 'eu',
+    encrypted: true,
+});
+
+window.Echo.private(`user.${userId}`)
+    .listen('AlertNotification', (data) => {
+        $.notify({
+            message: 'Alert Triggered - ' + data.alert_name + '. The ' + data.alert_type + ' is currently ' + data.value,
+        },{
+            type: 'success',
+            placement: {
+                from: "bottom",
+                align: "right"
+            },
+            offset: 20,
+            spacing: 10,
+            delay: 0,
+            animate: {
+                enter: 'animated slideInUp',
+                exit: 'animated slideInDown'
+            },
+        });
+    });
+
+//window.Pusher.logToConsole = true;
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/app.css" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    @auth
+        <script>
+            window.userId = {{ \Illuminate\Support\Facades\Auth::user()->id }};
+        </script>
+    @endauth
     <script src="/js/app.js"></script>
 </head>
 <body>
