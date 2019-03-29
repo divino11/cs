bec22c2894584e4d8771af66c92cad5935eb875f
--get <a.vasyukov97@gmail.com>
2019-03-29 21:08:29 +0200

    update composer

--- database/migrations/2019_03_26_164132_add_frequency_column_and_expiration_date_and_drop_triggerings_limits_column_to_alerts.php
+++ database/migrations/2019_03_26_164132_add_frequency_column_and_expiration_date_and_drop_triggerings_limits_column_to_alerts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFrequencyColumnAndExpirationDateColumnAndDropTriggeringsLimitsColumnToAlerts extends Migration
+class AddFrequencyColumnAndExpirationDateAndDropTriggeringsLimitsColumnToAlerts extends Migration
 {
     /**
      * Run the migrations.
