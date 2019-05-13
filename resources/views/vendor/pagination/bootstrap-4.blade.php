c6f7e06243d6dedaf2545ca5b0f931d811655e61
--get <a.vasyukov97@gmail.com>
2019-05-13 17:24:16 +0300

    Update table in billing, tabs in account

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -33,26 +33,6 @@
             <li class="page-item d-none d-sm-block"><a class="page-link" href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
         @endif
 
-
-        {{-- Pagination Elements --}}
-        {{--@foreach ($elements as $element)
-            --}}{{-- "Three Dots" Separator --}}{{--
-            @if (is_string($element))
-                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
-            @endif
-
-            --}}{{-- Array Of Links --}}{{--
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
-                    @else
-                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach--}}
-
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item">
