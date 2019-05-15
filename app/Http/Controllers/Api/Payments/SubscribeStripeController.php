e1e837e23bb179eae2901ba57c949f09fccdac6a
--get <a.vasyukov97@gmail.com>
2019-05-15 18:05:10 +0300

    Create Facade

--- app/Http/Controllers/Api/Payments/SubscribeStripeController.php
+++ app/Http/Controllers/Api/Payments/SubscribeStripeController.php
@@ -8,36 +8,32 @@ use App\Transaction;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
-class SubscribeBraintreeController extends Controller
+class SubscribeStripeController extends Controller
 {
     public function __invoke(CreateRequest $request)
     {
-        if ($request->nonce) {
-            $request->user()->newSubscription('main', 'premium')->create($request->nonce);
+        if ($request->stripeToken) {
+
+            $request->user()->createAsStripeCustomer();
+            $request->user()->newSubscription('main', 'plan_F4XhvQQuBs8VaZ')->create($request->stripeToken);
 
             Transaction::updateOrCreate(['created_at' => Carbon::now()], [
                 'user_id' => $request->user()->id,
                 'description' => 'Subscription Pro',
                 'amount' => PaymentPrice::Subscription,
-                'service' => $request->type,
+                'service' => 'CreditCard',
                 'status' => 100,
             ]);
-            return response()->json([
-                'status' => back()->with('status', 'You have purchased pro subscription'),
-                'success' => true,
-            ]);
+            return redirect()->route('user.subscription.index')->with('status', 'You have purchased pro subscription');
         } else {
             Transaction::updateOrCreate(['created_at' => Carbon::now()], [
                 'user_id' => $request->user()->id,
                 'description' => 'Subscription Pro',
                 'amount' => PaymentPrice::Subscription,
-                'service' => $request->type,
+                'service' => 'CreditCard',
                 'status' => -1,
             ]);
-            return response()->json([
-                'status' => back()->with('status', 'Data is invalid'),
-                'success' => false,
-            ]);
+            return redirect()->route('user.subscription.index')->with('status', 'Data is invalid');
         }
     }
 }
--- resources/views/user/subscription/create.blade.php
+++ resources/views/user/subscription/create.blade.php
@@ -17,18 +17,20 @@
                     </div>
 
                     <div class="col-md-8 col-sm-8 advancedplan-module-right">
-                        <a href="#" class="btn btn-primary bt-section d-inline-flex" id="bitcoin-button" data-toggle="modal"
-                                data-target="#myModal"><i class="material-icons">credit_card</i>Credit card: $100
+                        <a href="#" class="btn btn-primary bt-section d-inline-flex" id="bitcoin-button"
+                           data-toggle="modal"
+                           data-target="#myModal"><i class="material-icons">credit_card</i>Credit card: $100
                         </a>
-                        <a href="{{ $link_transaction }}" class="btn btn-primary bt-section d-inline-flex" target="_blank"><i class="material-icons">monetization_on</i> Bitcoin: $100</a>
+                        <a href="{{ $link_transaction }}" class="btn btn-primary bt-section d-inline-flex"
+                           target="_blank"><i class="material-icons">monetization_on</i> Bitcoin: $100</a>
                     </div>
-
                 </div>
             </div>
             <!-- END blurb -->
 
             <div class="advancedplan-bottom">
-                <a class="btn btn-default bt-section-out" href="{{ route('user.subscription.index') }}" role="button">Back to all plans</a>
+                <a class="btn btn-default bt-section-out" href="{{ route('user.subscription.index') }}" role="button">Back
+                    to all plans</a>
             </div>
 
         </div>
@@ -46,14 +48,23 @@
                             <h4 class="modal-title" id="myModalLabel">Pay with Card</h4>
                         </div>
                         <div class="modal-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div id="dropin-container"></div>
+                            <form action="{{ route('api.payments.stripe.subscribe') }}" method="post" id="payment-form">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-row">
+                                            <label for="card-element">
+                                                Credit or debit card
+                                            </label>
+                                            <div id="card-element"></div>
+                                            <div id="card-errors" role="alert"></div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button class="btn btn-primary bt-section   " id="dropin-button">Pay 100$</button>
-                            </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-primary bt-section" type="submit">Pay 100$</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -63,30 +74,75 @@
 @endsection
 
 @push('scripts')
-    <script src="https://js.braintreegateway.com/web/dropin/1.14.1/js/dropin.min.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
     <script>
-        var submitButton = document.querySelector('#dropin-button');
+        var stripe = Stripe('pk_test_oah3rY56CrjlAj47rf1WKwse');
+        // Create an instance of Elements.
+        var elements = stripe.elements();
 
-        braintree.dropin.create({
-            authorization: "{{ Braintree_ClientToken::generate() }}",
-            selector: '#dropin-container',
-            paypal: {
-                flow: 'vault'
+        // Custom styling can be passed to options when creating an Element.
+        // (Note that this demo uses a wider set of styles than the guide below.)
+        var style = {
+            base: {
+                color: '#32325d',
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#fa755a',
+                iconColor: '#fa755a'
             }
-        }, function (createErr, instance) {
-            submitButton.addEventListener('click', function () {
-                $('#dropin-button').attr('disabled', 'disabled').text('Loading ...');
-                instance.requestPaymentMethod(function (err, payload) {
-                    $.get('{{ route('api.payments.braintree.subscribe') }}', payload, function (response) {
-                        if (response.success) {
-                            $('#dropin-button').remove();
-                            window.location.href = '{{ route('user.subscription.index') }}';
-                        } else {
-                            alert('Payment failed. Please contact us');
-                        }
-                    }, 'json');
-                });
+        };
+
+        // Create an instance of the card Element.
+        var card = elements.create('card', {hidePostalCode: true, style: style});
+
+        // Add an instance of the card Element into the `card-element` <div>.
+        card.mount('#card-element');
+
+        // Handle real-time validation errors from the card Element.
+        card.addEventListener('change', function (event) {
+            var displayError = document.getElementById('card-errors');
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        // Handle form submission.
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            stripe.createToken(card).then(function (result) {
+                if (result.error) {
+                    // Inform the user if there was an error.
+                    var errorElement = document.getElementById('card-errors');
+                    errorElement.textContent = result.error.message;
+                } else {
+                    // Send the token to your server.
+                    stripeTokenHandler(result.token);
+                }
             });
         });
+
+        // Submit the form with the token ID.
+        function stripeTokenHandler(token) {
+            // Insert the token ID into the form so it gets submitted to the server
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type', 'hidden');
+            hiddenInput.setAttribute('name', 'stripeToken');
+            hiddenInput.setAttribute('value', token.id);
+            form.appendChild(hiddenInput);
+
+            // Submit the form
+            form.submit();
+        }
     </script>
 @endpush
